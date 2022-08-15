24abe8276f22a9eae2bfa27985f74cf4b569200e
Elise <elisegrizzell@gmail.com>
2022-08-15 11:08:14 -0500

    Added color palette input examples

--- components.py
+++ components.py
@@ -1,4 +1,24 @@
 import webcolors
+
+#Example Input from coolers.co export palette
+"""
+Object
+{"Charcoal": "264653", "Persian Green": "2a9d8f", "Maize Crayola": "e9c46a", "Sandy Brown": "f4a261", "Burnt Sienna": "e76f51"}
+
+/* Extended Array */
+[{"name":"Charcoal","hex":"264653","rgb":[38,70,83],"cmyk":[54,16,0,67],"hsb":[197,54,33],"hsl":[197,37,24],"lab":[28,-8,-11]},{"name":"Persian Green","hex":"2a9d8f","rgb":[42,157,143],"cmyk":[73,0,9,38],"hsb":[173,73,62],"hsl":[173,58,39],"lab":[59,-35,-2]},{"name":"Maize Crayola","hex":"e9c46a","rgb":[233,196,106],"cmyk":[0,16,55,9],"hsb":[43,55,91],"hsl":[43,74,66],"lab":[81,2,50]},{"name":"Sandy Brown","hex":"f4a261","rgb":[244,162,97],"cmyk":[0,34,60,4],"hsb":[27,60,96],"hsl":[27,87,67],"lab":[74,24,46]},{"name":"Burnt Sienna","hex":"e76f51","rgb":[231,111,81],"cmyk":[0,52,65,9],"hsb":[12,65,91],"hsl":[12,76,61],"lab":[61,44,38]}]
+
+/* XML */
+<palette>
+  <color name="Charcoal" hex="264653" r="38" g="70" b="83" />
+  <color name="Persian Green" hex="2a9d8f" r="42" g="157" b="143" />
+  <color name="Maize Crayola" hex="e9c46a" r="233" g="196" b="106" />
+  <color name="Sandy Brown" hex="f4a261" r="244" g="162" b="97" />
+  <color name="Burnt Sienna" hex="e76f51" r="231" g="111" b="81" />
+</palette>
+"""
+
+
 class PaletteColor:
     def __init__(self, color_hex, name=None, color_categories=[]):
         self.name = name
