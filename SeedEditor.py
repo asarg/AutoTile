f4bc84aaacfa7c1e313ee30b0b632d7f71029eef
Andrew Rodriguez <aroday23@gmail.com>
2022-09-24 14:49:34 -0500

    Add basic seed editor window
    SeedEditor.py is a basic QWindow I am using for "designing" the seed
    editor. Currently we can draw the assembly to this window. Next up is
    writing the click handler to click on the tiles. Here is a little
    roadmap.
    
    Basic click handler -- Click on a tile, and it prints to cmdline
    Add dummy tiles -- Places where tiles can go
    Selecting tiles to paint -- Listing seed states
    Advance click handler -- Click to select, Click to paste in empty spot
    Non-git merge -- Move this code to the actual seed editor
    Git merge -- Merge code

---
+++ SeedEditor.py
@@ -0,0 +1,117 @@
+import sys
+
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import Qt
+from UniversalClasses import Assembly, Tile, State
+
+
+class TestWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("TestWindow")
+
+        # Seed Editor Code
+        self.width: int = 800
+        self.height: int = 600
+        self.resize(self.width, self.height)
+        self.seedX = self.geometry().width() / 2
+        self.seedY = self.geometry().height() / 2
+        self.label = QtWidgets.QLabel()
+        canvas = QtGui.QPixmap(self.width, self.height)
+        # canvas.fill(Qt.white)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+
+        self.tileSize = 40
+        self.textSize = int(self.tileSize / 3)
+
+        # Testing Assembly
+        s1 = State("yo", "ff0000")
+        s2 = State("ma", "00ff00")
+        s3 = State("ya", "0000ff")
+        tiles = []
+        tiles.append(Tile(s1, 0, 0))
+        tiles.append(Tile(s2, 1, 0))
+        tiles.append(Tile(s3, 0, 1))
+        ass = Assembly()
+        ass.setTilesFromList(tiles)
+        self.draw_assembly(ass)
+
+    def onScreen_check(self, x, y):
+        if((x * self.tileSize) + self.seedX > self.geometry().width() or (x * self.tileSize) + self.seedX < -self.tileSize):
+            return 1
+        if((y * -self.tileSize) + self.seedY > self.geometry().height() or (y * -self.tileSize) + self.seedY < -self.tileSize):
+            return 1
+        return 0
+
+    def draw_to_screen(self, x, y, state, painter, brush):
+        painter.setBrush(brush)
+        ts = self.tileSize
+        ts_x = int((x * ts) + self.seedX)
+        ts_y = int((y * -ts) + self.seedY)
+        rect = QtCore.QRect(ts_x, ts_y, ts, ts)
+
+        painter.drawRect(rect)
+
+        if state == "":
+            painter.drawText(rect, Qt.AlignCenter, "")
+            return
+        else:
+            decoded_display_label = state.display_label
+
+        if self.tileSize > 10:
+            fm = QtGui.QFontMetrics(painter.font())
+            txt_width = fm.width(decoded_display_label)
+
+            if decoded_display_label is None:
+                if len(state.label) > 4:
+                    painter.drawText(rect, Qt.AlignCenter, state.label[0:3])
+                else:
+                    painter.drawText(rect, Qt.AlignCenter, state.label)
+            elif len(decoded_display_label) > 4:
+
+                painter.drawText(rect, Qt.AlignCenter, decoded_display_label[0:3])
+            else:
+                painter.drawText(rect, Qt.AlignCenter, decoded_display_label)
+
+    def draw_assembly(self, assembly):
+        painter = QtGui.QPainter(self.label.pixmap())
+        pen = QtGui.QPen()
+        brush = QtGui.QBrush()
+        font = QtGui.QFont()
+
+        pen.setWidth(3)
+        brush.setStyle(Qt.SolidPattern)
+        pen.setColor(QtGui.QColor("white"))
+        brush.setColor(QtGui.QColor("white"))
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawRect(0, 0, self.geometry().width(),
+                         self.geometry().height())
+
+        # Font
+        font.setFamily("Fira Code")
+        font.setBold(True)
+        font.setPixelSize(self.textSize)
+        painter.setFont(font)
+
+        pen.setColor(QtGui.QColor("black"))
+        painter.setPen(pen)
+        for tile in assembly.tiles:
+            if self.onScreen_check(tile.x, tile.y) == 1:
+                continue
+
+            brush.setColor(QtGui.QColor("#" + tile.returnColor()))
+
+            self.draw_to_screen(tile.x, tile.y, tile.state, painter, brush)
+
+        painter.end()
+
+        self.update()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = TestWindow()
+window.show()
+app.exec_()
